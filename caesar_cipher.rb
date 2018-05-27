683e25bfc1e45a67662f9595d29128286b4dc29e
twinie96 <twinie96@gmail.com>
2018-05-27 10:22:03 +0200

    Finish Ruby part of tasks

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,28 @@
-class CeasarCipher
+class CaesarCipher
+  def initialize(string, shift = 13)
+    @string = string
+    alphabet = ('a'..'z').to_a.join
+    caps_alphabet = ('A'..'Z').to_a.join
+    i = shift % alphabet.size
+    @alphabet = alphabet
+    @encrypt = alphabet[i..-1] + alphabet[0...i]
+    @caps_alphabet = caps_alphabet
+    @caps_encrypt = caps_alphabet[i..-1] + caps_alphabet[0...i]
+  end
+
+  def perform()
+    @result = ""
+    chars = @string.split('')
+    chars.each do |letter|
+      if @alphabet.include? letter
+        @result << letter.tr(@alphabet, @encrypt)
+      elsif @caps_alphabet.include? letter
+        @result << letter.tr(@caps_alphabet, @caps_encrypt)
+      else
+        @result << letter
+      end
+    end
+    @result
+  end
 
 end
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,35 @@
 class PassedOrFailed
 
+  def initialize (results, treshold)
+    @hash = results
+    @score = treshold.to_i
+    @pass = Hash.new
+    @fail = Hash.new
+    # unless @score.is_a? Integer
+    #     raise "Input error - score must be an integer"
+    # end
+  end
+
+  def iterate
+    @hash.each do |key, value|
+      if value.to_i >= @score
+        @pass[key] = value.to_i
+      else
+        @fail[key] = value.to_i
+      end
+    end
+  end
+
+  def perform
+    iterate
+    res = Hash.new
+    res[:passed]=@pass
+    res[:failed]=@fail
+    res
+  end
+
 end
+#hash = {"Mark" => 10, "Ellen" => 65, "Roger" => 20, "Mike" => 70}
+
+
+#PassedOrFailed.new(hash, 10).result
